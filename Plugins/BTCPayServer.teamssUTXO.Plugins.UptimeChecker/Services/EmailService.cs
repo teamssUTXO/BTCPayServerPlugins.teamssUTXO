e037389c49c5bc2b7f5d07ce129470a769d85f80
Timothé <timothe.fardella@viacesi.fr>
2026-03-05 16:16:23 +0100

    chore : create services's skeletton

---
+++ Plugins/BTCPayServer.teamssUTXO.Plugins.UptimeChecker/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace BTCPayServer.teamssUTXO.Plugins.UptimeChecker.Services;
+
+public class EmailService
+{
+    // service qui va envoyer les mails
+
+    public EmailService() {}
+
+    public async Task<bool> sendMailDown()
+    {
+        // méthode qui envoie un mail lorqu'un service est down
+        return true;
+    }
+
+    public async Task<bool> sendMailUp()
+    {
+        // méthode qui envoie un mail lorsqu'un service est up (après un down)
+        return true;
+    }
+}
