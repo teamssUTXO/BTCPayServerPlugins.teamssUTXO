80495003d12122ece4956bd9afe7407fe9a7e077
Timothé <timothe.fardella@viacesi.fr>
2026-03-23 19:04:49 +0100

    fix : antiforgery

--- Plugins/BTCPayServer.teamssUTXO.Plugins.UptimeChecker/Controllers/UptimeCheckerController.cs
+++ Plugins/BTCPayServer.teamssUTXO.Plugins.UptimeChecker/Controllers/UptimeCheckerController.cs
@@ -14,6 +14,7 @@ namespace BTCPayServer.teamssUTXO.Plugins.UptimeChecker.Controllers;
 
 
 [Route("server/uptimechecker")]
+[AutoValidateAntiforgeryToken]
 [Authorize(Policy = Policies.CanModifyServerSettings, AuthenticationSchemes = AuthenticationSchemes.Cookie)]
 public class UptimeCheckerController(UptimeCheckerService uptimeCheckerService, ChecksHistoryService checksHistoryService) : Controller
 {
